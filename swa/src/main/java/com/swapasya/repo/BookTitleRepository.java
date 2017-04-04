570a3457e30971f506ecbf33c0bd427e126ab254
Mallinath <Mallinath@Mallinath-PC>
2017-04-04 22:40:48 +0530

    Repository added

--- swa/src/main/java/com/swapasya/repo/BookTitleRepository.java
+++ swa/src/main/java/com/swapasya/repo/BookTitleRepository.java
@@ -1,12 +1,13 @@
 package com.swapasya.repo;
 
-import java.lang.annotation.Annotation;
+import java.io.Serializable;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.Assert;
 
@@ -14,32 +15,6 @@ import com.swapasya.domains.BookTitle;
 
 @Repository
 @Profile("mongodb")
-public class BookTitleRepository {
+public interface BookTitleRepository  extends CrudRepository<BookTitle, Long>{
 	
-	private final MongoOperations operations;
-	
-	/**
-	 * Creates a new {@link MongoDbCustomerRepository} using the given {@link MongoOperations}.
-	 * 
-	 * @param operations must not be {@literal null}.
-	 */
-	@Autowired
-	public BookTitleRepository(MongoOperations operations) {
-
-		Assert.notNull(operations);
-		this.operations = operations;
-	}
-	
-	public BookTitle findOne(Long id) {
-		Query query = Query.query(Criteria.where("id").is(id));
-		return operations.findOne(query, BookTitle.class);
-	}
-	
-	public BookTitle save(BookTitle bookTitle) {
-		operations.save(bookTitle);
-		return bookTitle;
-	}
-	
-	
-
 }
