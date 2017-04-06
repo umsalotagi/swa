d6e2166ea4a1ff71b3883cb9ab829e3e2b534c84
Umesh <Umesh>
2017-04-07 00:19:01 +0530

    Change-Id: I49861d76f8c8db4b954838197ba68197147b4d9a

--- swa/src/main/java/com/swapasya/domains/Book.java
+++ swa/src/main/java/com/swapasya/domains/Book.java
@@ -4,7 +4,6 @@ import java.util.Date;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 
