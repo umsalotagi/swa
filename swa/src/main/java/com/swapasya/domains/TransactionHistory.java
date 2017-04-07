cf86dd5d25b5a99f1cfe34d63e860b495e71cb52
Umesh <Umesh>
2017-04-08 01:00:21 +0530

    Change-Id: I23c032274eceb4ca5400be710b60280edd1d77ce

--- swa/src/main/java/com/swapasya/domains/TransactionHistory.java
+++ swa/src/main/java/com/swapasya/domains/TransactionHistory.java
@@ -35,5 +35,42 @@ public class TransactionHistory {
 	private String authour ;
 	// role of issuer
 	
+	/**
+	 * @param transactionID
+	 * @param issuetype
+	 * @param dateOfIssue
+	 * @param dateOfReturn
+	 * @param bookID
+	 * @param bookTitleID
+	 * @param fineCollected
+	 * @param personID
+	 * @param courseyear
+	 * @param branch
+	 * @param degree
+	 * @param renewIndex
+	 * @param bookName
+	 * @param authour
+	 */
+	public TransactionHistory(String transactionID, String issuetype, Date dateOfIssue, Date dateOfReturn,
+			String bookID, String bookTitleID, double fineCollected, String personID, int courseyear, String branch,
+			String degree, String renewIndex, String bookName, String authour) {
+		super();
+		this.transactionID = transactionID;
+		this.issuetype = issuetype;
+		this.dateOfIssue = dateOfIssue;
+		this.dateOfReturn = dateOfReturn;
+		this.bookID = bookID;
+		this.bookTitleID = bookTitleID;
+		this.fineCollected = fineCollected;
+		this.personID = personID;
+		this.courseyear = courseyear;
+		this.branch = branch;
+		this.degree = degree;
+		this.renewIndex = renewIndex;
+		this.bookName = bookName;
+		this.authour = authour;
+	}
+	
+	
 	
 }
