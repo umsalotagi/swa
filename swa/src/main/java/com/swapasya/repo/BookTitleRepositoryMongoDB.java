384e679d5d328d8478487a6715a494e600565a2f
Umesh <Umesh>
2017-04-24 23:41:37 +0530

    small change

--- swa/src/main/java/com/swapasya/repo/BookTitleRepositoryMongoDB.java
+++ swa/src/main/java/com/swapasya/repo/BookTitleRepositoryMongoDB.java
@@ -65,13 +65,13 @@ public class BookTitleRepositoryMongoDB implements BookTitleRepository {
 	@Override
 	public void delete(BookTitle bookTitle) {
 		operations.remove(bookTitle);
-		
+
 	}
 
 	@Override
 	public void delete(Iterable<? extends BookTitle> iterable) {
 		operations.remove(iterable);
-		
+
 	}
 
 	@Override
@@ -88,9 +88,9 @@ public class BookTitleRepositoryMongoDB implements BookTitleRepository {
 
 	@Override
 	public Iterable<BookTitle> findAll() {
-		
+
 		return operations.findAll(BookTitle.class);
-		
+
 	}
 
 	@Override
@@ -144,103 +144,84 @@ public class BookTitleRepositoryMongoDB implements BookTitleRepository {
 		return operations.find(query, BookTitle.class);
 
 	}
-	
-	
+
 	@Override
 	public List<BookTitle> findByAuthor(String author) {
-		Query query = Query.query(Criteria.where("author").is(author)); // author ahe he authour lihili tu
+		Query query = Query.query(Criteria.where("author").is(author)); 
 		return operations.find(query, BookTitle.class);
 	}
 
 	@Deprecated
 	public List<BookTitle> findByAuthor2(String author) {
-//		Query query = Query.query(Criteria.where("authour").is(author));
+		// Query query = Query.query(Criteria.where("authour").is(author));
 		DBCollection coll = operations.getCollection("BookTitle");
-List<Object> listOfObjects=new ArrayList<>();
-List<BookTitle> listOfbkTitles=new ArrayList<>();
+		List<Object> listOfObjects = new ArrayList<>();
+		List<BookTitle> listOfbkTitles = new ArrayList<>();
 		BasicDBObject key = new BasicDBObject("bookTitleID", true);
 		key.append("imgPath", true);
 		key.append("bookName", true);
-		key.append("author",true);
-		key.append("publication",true);
-	    BasicDBObject initial = new BasicDBObject("","");
-try {
+		key.append("author", true);
+		key.append("publication", true);
+		BasicDBObject initial = new BasicDBObject("", "");
+		try {
 
+			BasicDBObject cond = new BasicDBObject();
+			cond.put("author", author);
 
-	        BasicDBObject cond = new BasicDBObject();
-	        cond.put("author", author);
+			String reduce = "function(doc,prev){ }";
 
-	        String reduce = "function(doc,prev){ }";
-	        
-	        BasicDBList returnList = (BasicDBList) coll.group(key, cond, initial, reduce);
-	        
-	            for (Object o : returnList) {
-	            	System.out.println(o);
-	            	
+			BasicDBList returnList = (BasicDBList) coll.group(key, cond, initial, reduce);
 
-	                if (o instanceof Map) {
-	                    Map<String,BookTitle> map = (Map) o;
-	                    for (Object obj2 : map.entrySet()) {
-	                    	
-	                        if (obj2 instanceof Entry) {
-	                            Entry entry = (Entry) obj2;
-	                           //System.out.println(entry.getValue());
-	       listOfObjects.add(entry.getValue());                 }
-	                    }
-	                    
-	                    BookTitle bkTitle=new BookTitle();
-	                    
-	                    bkTitle.setBookTitleID(listOfObjects.get(0).toString());
-	                    
-	                    bkTitle.setImgPath("./images/black_book.jpg");
-	                    bkTitle.setBookName(listOfObjects.get(2).toString());
-	                    bkTitle.setAuthor(listOfObjects.get(3).toString());
-	                    bkTitle.setPublication(listOfObjects.get(4).toString());
-	                    listOfObjects.clear();
-	                    listOfbkTitles.add(bkTitle);
-	                }
+			for (Object o : returnList) {
+				System.out.println(o);
 
-	                
-	            }
-	            	
-	            	for(int j=0;j<listOfbkTitles.size();j++)
-	            	{
-	            		System.out.println(listOfbkTitles.get(j));
-	            	}
-	            
-	            
-	                /*
-	            Map<String, BookTitle> map=returnList.toMap();
-	            	Set<String> set=map.keySet();
-	            Iterator<String> i=set.iterator();
-	            while(i.hasNext())
-	            {
-	            	BookTitle title= map.get(i.next());
-	            	System.out.println(title);
-	            }
-	            	*/
-	         
-	        
-	        
-	        
-	        
-	        
-	        /*
-	        
-	        Iterator<Object> i=returnList.iterator();
-	            while(i.hasNext())
-	            {
-	            	BookTitle bkTitle=(BookTitle) i.next();
-	            	System.out.println(bkTitle.getAuthour()+bkTitle.getBookName());
-	            }
-	            
-	          */  
-}catch (Exception e) {
-	e.printStackTrace();
-}
+				if (o instanceof Map) {
+					Map<String, BookTitle> map = (Map) o;
+					for (Object obj2 : map.entrySet()) {
 
+						if (obj2 instanceof Entry) {
+							Entry entry = (Entry) obj2;
+							// System.out.println(entry.getValue());
+							listOfObjects.add(entry.getValue());
+						}
+					}
 
-		    
+					BookTitle bkTitle = new BookTitle();
+
+					bkTitle.setBookTitleID(listOfObjects.get(0).toString());
+
+					bkTitle.setImgPath("./images/black_book.jpg");
+					bkTitle.setBookName(listOfObjects.get(2).toString());
+					bkTitle.setAuthor(listOfObjects.get(3).toString());
+					bkTitle.setPublication(listOfObjects.get(4).toString());
+					listOfObjects.clear();
+					listOfbkTitles.add(bkTitle);
+				}
+
+			}
+
+			for (int j = 0; j < listOfbkTitles.size(); j++) {
+				System.out.println(listOfbkTitles.get(j));
+			}
+
+			/*
+			 * Map<String, BookTitle> map=returnList.toMap(); Set<String>
+			 * set=map.keySet(); Iterator<String> i=set.iterator();
+			 * while(i.hasNext()) { BookTitle title= map.get(i.next());
+			 * System.out.println(title); }
+			 */
+
+			/*
+			 * 
+			 * Iterator<Object> i=returnList.iterator(); while(i.hasNext()) {
+			 * BookTitle bkTitle=(BookTitle) i.next();
+			 * System.out.println(bkTitle.getAuthour()+bkTitle.getBookName()); }
+			 * 
+			 */
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		return listOfbkTitles;
 	}
 
@@ -257,7 +238,7 @@ try {
 		return operations.find(query, BookTitle.class);
 
 	}
-	
+
 	@Override
 	public void insertOne(BookTitle bookTitle) {
 		operations.insert(bookTitle);
@@ -265,13 +246,13 @@ try {
 
 	@Deprecated
 	public void insertOne2(BookTitle bookTitle) {
-		
-	//	operations.in
-		
-	    DBCollection coll=  operations.getCollection("BookTitle");
-	    
-		BasicDBObject bkTitle=new BasicDBObject();
-		
+
+		// operations.in
+
+		DBCollection coll = operations.getCollection("BookTitle");
+
+		BasicDBObject bkTitle = new BasicDBObject();
+
 		bkTitle.put("bookTitleID", bookTitle.getBookTitleID());
 		bkTitle.put("isbnNumber", bookTitle.getIsbnNumber());
 		bkTitle.put("bookName", bookTitle.getBookName());
@@ -282,19 +263,16 @@ try {
 		bkTitle.put("language", bookTitle.getLanguage());
 		bkTitle.put("imgPath", bookTitle.getImgPath());
 		bkTitle.put("tags", bookTitle.getTags().toArray());
-		
-		
-		
-		List<Book> listOfBooks= bookTitle.getBooks();
-		BasicDBObject book[]=new BasicDBObject[listOfBooks.size()];
-		int j=0;
-		Iterator<Book> i= listOfBooks.iterator();
-		while(i.hasNext())
-		{
-			Book b=i.next();
-			
-			book[j]=new BasicDBObject();
-			
+
+		List<Book> listOfBooks = bookTitle.getBooks();
+		BasicDBObject book[] = new BasicDBObject[listOfBooks.size()];
+		int j = 0;
+		Iterator<Book> i = listOfBooks.iterator();
+		while (i.hasNext()) {
+			Book b = i.next();
+
+			book[j] = new BasicDBObject();
+
 			book[j].put("bookID", b.getBookID());
 			book[j].put("purchaseDate", b.getPurchaseDate());
 			book[j].put("price", b.getPrice());
@@ -304,69 +282,35 @@ try {
 			book[j].put("issueDate", b.getIssueDate());
 			book[j].put("expectedReturnDate", b.getExpectedReturnDate());
 			j++;
-			
+
 		}
-		bkTitle.put("books",book);
+		bkTitle.put("books", book);
 		System.out.println("Till books done");
-		
-		BasicDBObject waitLs=new BasicDBObject();
-		List<WaitList> waitList= bookTitle.getWaitList();
-		Iterator<WaitList> i1= waitList.iterator();
-		while(i1.hasNext())
-		{
-			WaitList w=i1.next();
+
+		BasicDBObject waitLs = new BasicDBObject();
+		List<WaitList> waitList = bookTitle.getWaitList();
+		Iterator<WaitList> i1 = waitList.iterator();
+		while (i1.hasNext()) {
+			WaitList w = i1.next();
 			waitLs.put("id", w.getId());
-			waitLs.put("timestamp",w.getTimestamp());
+			waitLs.put("timestamp", w.getTimestamp());
 			bkTitle.put("waitList", waitLs);
 		}
-		
+
 		System.out.println("Till waitList");
-		
-		BasicDBObject assignLs=new BasicDBObject();
-		List<AssignList> assignList= bookTitle.getAssignList();
-		Iterator<AssignList> i2= assignList.iterator();
-		while(i2.hasNext())
-		{
-			AssignList a=i2.next();
+
+		BasicDBObject assignLs = new BasicDBObject();
+		List<AssignList> assignList = bookTitle.getAssignList();
+		Iterator<AssignList> i2 = assignList.iterator();
+		while (i2.hasNext()) {
+			AssignList a = i2.next();
 			assignLs.put("id", a.getId());
-			assignLs.put("timestamp",a.getTimestamp());
+			assignLs.put("timestamp", a.getTimestamp());
 			bkTitle.put("assignList", assignList);
 		}
 		System.out.println("Till assignList");
-		 coll.insert(bkTitle);
-		
-		}
-		
-		
-		
+		coll.insert(bkTitle);
+
 	}
-	
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
