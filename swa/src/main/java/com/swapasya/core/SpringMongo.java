8d24bdd603760b607f7731fa6f6a7665b88a424b
Umesh <Umesh>
2017-04-27 23:35:14 +0530

    Change-Id: I9db30b253e7808541c8b7c454a2b673b7d7e0d21

--- swa/src/main/java/com/swapasya/core/SpringMongo.java
+++ swa/src/main/java/com/swapasya/core/SpringMongo.java
@@ -9,103 +9,67 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 
 import com.mongodb.MongoClient;
+import com.swapasya.dataTypes.BookTitleProp;
 import com.swapasya.domains.Book;
 import com.swapasya.domains.BookTitle;
+import com.swapasya.model.DBConnect;
+import com.swapasya.repo.BookTitleRepositoryMongoDB;
 
 public class SpringMongo {
-	
-	
-MongoOperations mongoOps;
-	
-	public SpringMongo(String nameSpace) {
 
-		try{
-			
-	         // To connect to mongodb server
-			MongoClient mongoClient = new MongoClient("localhost", 27017);
-	         	
-	         // Now connect to your databases
+	MongoOperations mongoOps;
 
-	         mongoOps = new MongoTemplate(mongoClient, "test");
-	         System.out.println("Connect to database successfully");
-	    //     boolean auth = db.authenticate(myUserName, myPassword);
-	    //     System.out.println("Authentication: "+auth);
+	public SpringMongo() {
 
-	      }catch(Exception e){
-	         System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-	      }
+		try {
+
+			mongoOps = DBConnect.getConnection();
+			System.out.println("Connect to database successfully");
+
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		}
 
 	}
-	
-	
+
 	private void addBookTitleAndBook(String bookTitleID, String isbnNumber, String bookName, String authour,
 			String categoryType, String publication, float price, Date purchaseDate, String bindingType,
-			ArrayList<String> tags, int noOfPages, String language, String bookID ) {
-		
-		BookTitle bt = new BookTitle(bookTitleID, isbnNumber, bookName, authour, 
-				publication, bindingType, tags, noOfPages, language);
-		
-		Book b = new Book(bookID, purchaseDate, price, categoryType);
-		
-		if (bt!=null) {
-			bt.getBooks().add(b);
-			
-			mongoOps.insert(bt);
-		}
-		
+			ArrayList<String> tags, int noOfPages, String language, String bookID) {
 
-	}
-	
-	private void addBookTitle(String bookTitleID, String isbnNumber, String bookName, String authour,
-			String categoryType, String publication, float price, Date purchaseDate, String bindingType,
-			ArrayList<String> tags, int noOfPages, String language) {
-		
-		BookTitle bt = new BookTitle(bookTitleID, isbnNumber, bookName, authour, 
-				publication, bindingType, tags, noOfPages, language);
-		
-		
-		mongoOps.insert(bt);
-
-	}
-	
-	// if personID does not exists :
-	// KeyFactory.createKey created any random key not null
-	// datastore.get throws exception EntityNotFoundException
-	
-	// if property does not exists it is returns null ( no exception thrown)
-	
-	private void addBookInBookTitle(BookTitle bt, String bookID, String bookName, String authour,
-			String categoryType, float price, Date purchaseDate) {
+		BookTitle bt = new BookTitle(bookTitleID, isbnNumber, bookName, authour, publication, bindingType, tags,
+				noOfPages, language);
 
 		Book b = new Book(bookID, purchaseDate, price, categoryType);
-		
-		if (bt!=null) {
+
+		if (bt != null) {
 			bt.getBooks().add(b);
-			
+
 			mongoOps.insert(bt);
 		}
+
 	}
 
 	public void addBook(String bookID, String isbnNumber, String bookName, String authour, String categoryType,
 			String publication, float price, Date purchaseDate, String bindingType, ArrayList<String> tags,
 			int noOfPages, String language) {
-		
-		
+
 		Query q = new Query();
-		q.addCriteria(new Criteria().where("bookName").is(bookName));
-		q.addCriteria(new Criteria().where("authour").is(authour));
-		
-		BookTitle bt = mongoOps.findOne(q , BookTitle.class);
-		
-		if (bt==null) {
+		q.addCriteria(Criteria.where("bookName").is(bookName));
+		q.addCriteria(Criteria.where("authour").is(authour));
+
+		BookTitle bt = mongoOps.findOne(q, BookTitle.class);
+
+		if (bt == null) {
 			String bookTitleID = "T_" + bookID;
-			addBookTitleAndBook(bookTitleID, isbnNumber, bookName, authour, categoryType, publication, price, 
+			addBookTitleAndBook(bookTitleID, isbnNumber, bookName, authour, categoryType, publication, price,
 					purchaseDate, bindingType, tags, noOfPages, language, bookID);
 		} else {
-			addBookInBookTitle(bt, bookID, bookName, authour, categoryType, price, purchaseDate);
+			Book bb = new Book(bookID, purchaseDate, price, categoryType);
+			bt.addBook(bb);
+			new BookTitleRepositoryMongoDB(mongoOps).save(bt);
+
 		}
-		
+
 	}
-	
 
 }
